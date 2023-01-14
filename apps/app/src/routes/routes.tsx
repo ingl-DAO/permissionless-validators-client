eadbde3be71e6e204ca4909a66607a4aeec5fbb2
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-01-14 19:17:22 +0100

    modified routes file configuring validator routes

--- apps/app/src/routes/routes.tsx
+++ apps/app/src/routes/routes.tsx
@@ -1,12 +1,30 @@
+import { Typography } from '@mui/material';
 import { Navigate } from 'react-router';
 import Home from '../pages';
 import Layout from '../pages/layout';
+import ValidatorLayout from '../pages/[vote_account_id]';
+import ValidatorNFTs from '../pages/[vote_account_id]/nfts';
 
 export const routes = [
+  { path: '/', element: <Typography>Authenticate user</Typography> },
   {
-    path: '/',
+    path: '',
     element: <Layout />,
-    children: [{ path: '', element: <Home /> }],
+    children: [
+      { path: 'validators', element: <Home /> },
+      {
+        path: 'validators/:vote_account_id',
+        element: <ValidatorLayout />,
+        children: [
+          { path: '', element: <Navigate to="nfts" /> },
+          { path: 'nfts', element: <ValidatorNFTs /> },
+        ],
+      },
+      {
+        path: 'register-validator',
+        element: <Typography>Testing works</Typography>,
+      },
+    ],
   },
   { path: '*', element: <Navigate to="/" /> },
 ];
