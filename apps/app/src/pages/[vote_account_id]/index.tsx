423851b5602417e17fa8b70f4065795e5a98b8ba
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-01-14 22:07:25 +0100

    styling to validator layout

--- apps/app/src/pages/[vote_account_id]/index.tsx
+++ apps/app/src/pages/[vote_account_id]/index.tsx
@@ -1,10 +1,11 @@
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router';
 import Navbar from '../../components/navbar';
+import theme from '../../theme/theme';
 
 export default function ValidatorLayout() {
   return (
-    <Box sx={{ display: 'grid' }}>
+    <Box sx={{ display: 'grid', rowGap: theme.spacing(2.375) }}>
       <Navbar />
       <Box>
         <Outlet />
