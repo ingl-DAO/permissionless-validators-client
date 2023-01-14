ea35866e55642a94ae9eb894222107ff48df2f9b
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-01-14 19:16:18 +0100

    added interfaces. to centralise the presence of validators

---
+++ apps/app/src/interfaces.ts
@@ -0,0 +1,9 @@
+export interface Validator {
+  validator_name: string;
+  validator_website: string;
+  vote_account_id: string;
+  nft_share: number;
+  total_requested_stake: number;
+  apy: number;
+  image_ref: string;
+}
