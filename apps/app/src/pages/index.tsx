2a03b6b00ea01e900c00234b478df6a4a8e8ed82
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-01-14 19:18:11 +0100

    display all validators

--- apps/app/src/pages/index.tsx
+++ apps/app/src/pages/index.tsx
@@ -1,5 +1,31 @@
 import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import Scrollbars from 'rc-scrollbars';
+import { useState } from 'react';
+import HomeSearchBar from '../components/homeSearchBar';
+import AllValidators from '../components/validators/validators';
+import theme from '../theme/theme';
 
 export default function Home() {
-  return <Typography>Hello world</Typography>;
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        rowGap: theme.spacing(4.375),
+        height: '100%',
+        gridTemplateRows: 'auto auto 1fr',
+      }}
+    >
+      <HomeSearchBar
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      <Typography variant="h5">Permissionless Validators</Typography>
+      <Scrollbars autoHide>
+        <AllValidators searchValue={searchValue} />
+      </Scrollbars>
+    </Box>
+  );
 }
