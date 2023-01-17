c4a9278308aa4f32102205f5261ac62093a0bc54
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-01-17 04:02:13 +0100

    added expand card

--- apps/app/src/components/nft/nftCard.tsx
+++ apps/app/src/components/nft/nftCard.tsx
@@ -1,7 +1,8 @@
-import { MoreHorizRounded } from '@mui/icons-material';
+import { MoreHorizRounded, OpenInFullOutlined } from '@mui/icons-material';
 import {
   Box,
   Button,
+  Dialog,
   IconButton,
   Menu,
   MenuItem,
@@ -13,7 +14,7 @@ import { InglNft } from '../../interfaces';
 import theme from '../../theme/theme';
 
 export default function NftCard({
-  gem: { rarity, image_ref, is_delegated, numeration },
+  gem: { rarity, image_ref, is_delegated, numeration, nft_pubkey },
   isDialogOpen,
   disabled,
   setActionnedNft,
@@ -70,17 +71,48 @@ export default function NftCard({
     setMenuAnchor(null);
   };
 
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
   return (
     <>
       <Box
         sx={{
+          position: 'relative',
           padding: theme.spacing(1.5),
           background:
             'linear-gradient(151.27deg, #EF233C 10.17%, #1C1C28 32.86%, #28293D 65.73%, #EF233C 150.23%)',
           borderRadius: theme.spacing(3.75),
           width: 'fit-content',
+          ':hover': {
+            '& .expand_icon': {
+              visibility: 'visible',
+            },
+          },
         }}
       >
+        <Tooltip arrow title={'Expand'}>
+          <Box
+            className="expand_icon"
+            onClick={() => setIsExpanded(true)}
+            sx={{
+              zIndex: 1,
+              visibility: 'hidden',
+              position: 'absolute',
+              height: '100px',
+              width: '100px',
+              backgroundColor: theme.palette.primary.main,
+              borderRadius: '100px',
+              top: '50%',
+              left: '50%',
+              margin: '-50px 0 0 -50px',
+              display: 'grid',
+              alignItems: 'center',
+              justifyItems: 'center',
+            }}
+          >
+            <OpenInFullOutlined sx={{ color: 'white', fontSize: '2.5rem' }} />
+          </Box>
+        </Tooltip>
         <Box
           sx={{
             background: 'rgba(9, 44, 76, 0.6)',
@@ -234,6 +266,97 @@ export default function NftCard({
           ) : null
         )}
       </Menu>
+      <Dialog
+        open={isExpanded}
+        onClose={() => setIsExpanded(false)}
+        sx={{
+          '& .MuiPaper-root': {
+            width: '50vw',
+            height: '50vh',
+            overflow: 'hidden',
+          },
+        }}
+      >
+        <Box
+          sx={{
+            background: 'rgba(9, 44, 76, 0.6)',
+            borderRadius: theme.spacing(0.5),
+            position: 'relative',
+            height: '100%',
+            width: '100%',
+          }}
+        >
+          <img
+            src={image_ref}
+            alt={`nft ${numeration}`}
+            style={{
+              objectFit: 'cover',
+              height: '100%',
+              width: '100%',
+              borderRadius: theme.spacing(0.5),
+            }}
+          />
+          <Box
+            sx={{
+              background: 'rgba(28, 28, 40, 0.75)',
+              position: 'absolute',
+              borderRadius: '20px 20px 0px 0px',
+              bottom: 0,
+              left: '50%',
+              transform: 'translate(-50%, 0%)',
+              padding: '10px',
+            }}
+          >
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: { laptop: 'initial', mobile: '0.80rem' },
+                textAlign: 'center',
+              }}
+            >
+              {`#${numeration}`}
+            </Typography>
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: { laptop: 'initial', mobile: '0.80rem' },
+                textAlign: 'center',
+                color: 'rgba(255, 255, 255, 0.5)',
+              }}
+            >
+              ID
+            </Typography>
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: { laptop: 'initial', mobile: '0.80rem' },
+                textAlign: 'center',
+              }}
+            >
+              {nft_pubkey}
+            </Typography>
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: { laptop: 'initial', mobile: '0.80rem' },
+                textAlign: 'center',
+                color: 'rgba(255, 255, 255, 0.5)',
+              }}
+            >
+              Rarity
+            </Typography>
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: { laptop: 'initial', mobile: '0.80rem' },
+                textAlign: 'center',
+              }}
+            >
+              {rarity}
+            </Typography>
+          </Box>
+        </Box>
+      </Dialog>
     </>
   );
 }
