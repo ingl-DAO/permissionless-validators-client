a0c5695be100d285852e1cac31a5d4e6c9ed85df
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-01-18 00:23:43 +0100

    added some utils for table functioning

---
+++ apps/app/src/common/components/noTableElement.tsx
@@ -0,0 +1,46 @@
+import { Skeleton, TableCell, TableRow } from '@mui/material';
+import theme from '../../theme/theme';
+
+export default function NoTableElement({
+  colSpan = 6,
+  message,
+}: {
+  colSpan?: number;
+  message: string;
+}) {
+  return (
+    <TableRow
+      sx={{
+        borderBottom: `1px solid ${theme.common.line}`,
+        borderTop: `1px solid ${theme.common.line}`,
+        padding: `0 ${theme.spacing(4.625)}`,
+      }}
+    >
+      <TableCell
+        colSpan={colSpan}
+        align="center"
+        sx={{ textAlign: 'center', color: 'white' }}
+      >
+        {message}
+      </TableCell>
+    </TableRow>
+  );
+}
+
+export function TableLaneSkeleton({ cols = 6 }: { cols?: number }) {
+  return (
+    <TableRow
+      sx={{
+        borderBottom: `1px solid ${theme.common.line}`,
+        borderTop: `1px solid ${theme.common.line}`,
+        padding: `0 ${theme.spacing(4.625)}`,
+      }}
+    >
+      {[...new Array(cols)].map((_, index) => (
+        <TableCell component="th" scope="row" key={index}>
+          <Skeleton animation="wave" />
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
