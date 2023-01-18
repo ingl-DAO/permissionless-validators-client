1983d9129a46fab983e7219edce31f3385b24a62
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-01-18 09:52:30 +0100

    added interface for validator stats and validator creation

--- apps/app/src/interfaces.ts
+++ apps/app/src/interfaces.ts
@@ -23,3 +23,37 @@ export interface NftReward {
   numeration: number;
   rewards: number;
 }
+
+export interface ValidatorMetaData {
+  rarities: number[];
+  rarity_names: string[];
+  twitter_handle: string;
+  discord_invite: string;
+}
+
+export interface ValidatorRegistration extends ValidatorMetaData {
+  proposal_quorum: number;
+  init_commission: number; //in percentage
+  total_delegated_stake: string; //in lamports (big number)
+  initial_redemption_fee: number;
+  is_validator_id_switchable: boolean;
+  unit_backing: string; //big number
+  redemption_fee_duration: number;
+  creator_royalties: number;
+  validator_name: string;
+  collection_uri: string;
+  nft_holders_share: number;
+  website: string;
+  governance_expiration_time: number;
+  default_uri: string;
+}
+export interface InglValidator
+  extends Omit<ValidatorRegistration, keyof ValidatorMetaData> {
+  validator_id: string;
+  validator_apy: number;
+  current_skip_rate: number;
+  vote_account_id: number;
+  collection_id: string;
+  total_delegated_count: number;
+  total_secondary_stake: string; // in lamports (big number)
+}
