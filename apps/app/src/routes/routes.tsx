34c8c5aa5fe8e0a9b8538efa19cdee77a2ceb222
Marco Kuidja <kuidjamarco@gmail.com>
2023-01-23 18:56:22 +0100

    setting fallback route to `/validators`

--- apps/app/src/routes/routes.tsx
+++ apps/app/src/routes/routes.tsx
@@ -1,16 +1,15 @@
-import { Typography } from '@mui/material';
 import { Navigate } from 'react-router';
 import Home from '../pages';
 import Layout from '../pages/layout';
+import Register from '../pages/register-validator';
 import ValidatorLayout from '../pages/[validator_program_id]';
 import ValidatorNFTs from '../pages/[validator_program_id]/nfts';
-import Register from '../pages/register-validator';
 import Rewards from '../pages/[validator_program_id]/rewards/index';
 import ValidatorStats from '../pages/[validator_program_id]/validatorStats';
 import Dao from '../pages/[vote_account_id]/dao';
 
 export const routes = [
-  { path: '/', element: <Typography>Authenticate user</Typography> },
+  { path: '/', element:  <Navigate to="/validators" /> },
   {
     path: '',
     element: <Layout />,
