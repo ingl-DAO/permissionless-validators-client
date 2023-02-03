4ccab1340b3e8713b6347b67c23b33c9448377cf
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-02-03 23:17:38 +0100

    extracted governance power as a component so as to use on proposal creation interfaces

--- apps/app/src/pages/[validator_program_id]/dao/index.tsx
+++ apps/app/src/pages/[validator_program_id]/dao/index.tsx
@@ -5,7 +5,7 @@ import {
   InputAdornment,
   Skeleton,
   TextField,
-  Typography,
+  Typography
 } from '@mui/material';
 import Scrollbars from 'rc-scrollbars';
 import { useEffect, useState } from 'react';
@@ -13,6 +13,7 @@ import { useIntl } from 'react-intl';
 import { useParams } from 'react-router';
 import ErrorMessage from '../../../common/components/ErrorMessage';
 import useNotification from '../../../common/utils/notification';
+import GovernancePower from '../../../components/dao/governancePower';
 import { GovernanceInterface, InglNft } from '../../../interfaces';
 import theme from '../../../theme/theme';
 import ProposalCard from './proposalCard';
@@ -287,102 +288,7 @@ export default function Dao() {
           <Button variant="contained" color="primary" fullWidth>
             New Proposal
           </Button>
-          <Box
-            sx={{
-              display: 'grid',
-              rowGap: theme.spacing(1),
-              backgroundColor: '#28293D',
-              padding: theme.spacing(1),
-              borderRadius: '15px',
-            }}
-          >
-            <Box
-              sx={{
-                display: 'grid',
-                gridTemplateColumns: '1fr auto',
-                columnGap: theme.spacing(1),
-              }}
-            >
-              <Typography variant="caption" sx={{ color: 'white' }}>
-                My governance power
-              </Typography>
-              <Typography
-                component={'span'}
-                variant="caption"
-                sx={{
-                  color: 'white',
-                  display: 'grid',
-                  gridAutoFlow: 'column',
-                  alignItems: 'center',
-                  justifyContent: 'end',
-                  columnGap: theme.spacing(2),
-                }}
-              >
-                {areNftsLoading ? (
-                  <Skeleton
-                    animation="wave"
-                    width={50}
-                    sx={{ backgroundColor: 'rgb(137 127 127 / 43%)' }}
-                  />
-                ) : (
-                  nfts.length
-                )}{' '}
-                NFTs
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                padding: theme.spacing(1),
-                backgroundColor: 'rgba(28, 28, 40, 1)',
-                borderRadius: '15px',
-                height: '92px',
-              }}
-            >
-              <Scrollbars autoHide>
-                <Box
-                  sx={{
-                    display: 'grid',
-                    gridAutoFlow: 'column',
-                    justifyContent: 'start',
-                    columnGap: theme.spacing(1.2),
-                  }}
-                >
-                  {areNftsLoading ? (
-                    [...new Array(10)].map((_, index) => (
-                      <Skeleton
-                        key={index}
-                        variant="rectangular"
-                        animation="wave"
-                        sx={{
-                          backgroundColor: 'rgb(137 127 127 / 43%)',
-                          borderRadius: theme.spacing(2),
-                        }}
-                        height={92}
-                        width={92}
-                      />
-                    ))
-                  ) : nfts.length === 0 ? (
-                    <Typography variant="caption">You own no Nft's</Typography>
-                  ) : (
-                    nfts.map(({ image_ref, numeration }, index) => (
-                      <img
-                        alt={`${numeration}`}
-                        src={image_ref}
-                        key={index}
-                        width="88px"
-                        height="88px"
-                        style={{
-                          objectFit: 'cover',
-                          borderRadius: theme.spacing(2),
-                          border: `2px solid ${theme.palette.primary.main}`,
-                        }}
-                      />
-                    ))
-                  )}
-                </Box>
-              </Scrollbars>
-            </Box>
-          </Box>
+          <GovernancePower areNftsLoading={areNftsLoading} nfts={nfts} />
         </Box>
       </Box>
     </Box>
