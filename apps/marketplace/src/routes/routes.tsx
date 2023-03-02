ade53c5728b6e2108bf288b2d824b1915afdb1fd
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-02 16:37:58 +0100

    added home to route file

--- apps/marketplace/src/routes/routes.tsx
+++ apps/marketplace/src/routes/routes.tsx
@@ -1,7 +1,17 @@
 import { Navigate } from 'react-router';
 import Layout from '../pages';
+import Home from '../pages/home';
 
 export const routes = [
-  { path: '/', element: <Layout /> },
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+    ],
+  },
   { path: '*', element: <Navigate to="/" /> },
 ];
