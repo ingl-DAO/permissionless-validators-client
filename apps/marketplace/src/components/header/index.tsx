a368becd5c8a3a0884f2005d27d7f74dd779f833
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-02 14:55:47 +0100

    added layout header

---
+++ apps/marketplace/src/components/header/index.tsx
@@ -0,0 +1,48 @@
+import { LocalMallOutlined } from '@mui/icons-material';
+import { Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router';
+import theme from '../../theme/theme';
+
+export default function Header() {
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: '1fr auto',
+        columnGap: 2,
+        padding: `0 ${theme.spacing(4.75)}`,
+        paddingTop: 4.25,
+      }}
+    >
+      <img
+        alt="ingl marketplace logo"
+        src={'/assets/full_logo.png'}
+        style={{ cursor: 'pointer' }}
+        onClick={() => navigate('/')}
+      />
+      <Box
+        sx={{ display: 'grid', gridTemplateColumns: 'auto auto', columnGap: 2 }}
+      >
+        <Button
+          startIcon={<LocalMallOutlined />}
+          variant="outlined"
+          color="primary"
+        >
+          My shopping bag
+        </Button>
+        <Button
+          sx={{
+            textTransform: 'none',
+            backgroundColor: '#2A2B40',
+            borderRadius: '15px',
+            boxShadow:
+              '0px 0px 1px rgba(239, 35, 60, 0.08), 0px 1px 2px rgba(239, 35, 60, 0.32)',
+          }}
+        >
+          Connect
+        </Button>
+      </Box>
+    </Box>
+  );
+}
